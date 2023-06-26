78b21bc158967b1e608ed56409a37d58cf7a9de0
Ajarek <ajarek@poczta.onet.pl>
2023-06-27 00:20:29 +0200

    added Component Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,32 @@
-
+import data from '../../assets/data.json'
 import './Home.css'
 
 const Home = () => {
-
   return (
     <div className='home'>
-      <h1>Home</h1>
+      {data.map((d) => {
+        return (
+          <div
+            key={d.id}
+            className='card'
+          >
+            <div className='card-wrapper'>
+              <div className='img'>
+                <img
+                  src={d.foto}
+                  alt='foto'
+                />
+              </div>
+              <div className='info'>
+                <h3>{d.nazwa}</h3>
+                <p>{d.kraj}</p>
+                <p>{d.rejon}</p>
+                <p>{d.cena}PLN</p>
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
